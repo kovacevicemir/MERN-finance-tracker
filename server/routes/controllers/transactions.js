eff138357d30b4c5a6c67de9973e4ffd16dbe1c9
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-02 02:14:32 +1000

    Transaction model added, and .get implemented in controller

--- server/routes/controllers/transactions.js
+++ server/routes/controllers/transactions.js
@@ -1,20 +1,34 @@
+const Transaction = require('../../models/Transaction')
+
 //@desc  Get all transactions
 //@route GET /api/v1/transactions
 //@access Public
-exports.getTransactions = (req,res,next) => {
-    res.send('get transactions')
+exports.getTransactions = async (req,res,next) => {
+    try {
+        const transactions = await Transaction.find()
+        return res.status(200).json({
+            succes:true,
+            count: transactions.length,
+            data: transactions
+        })
+    } catch (err) {
+        return res.send(500).json({
+            success:false,
+            error: 'server error'
+        })
+    }
 }
 
 //@desc  Add transaction
 //@route POST /api/v1/transactions
 //@access Public
-exports.addTransactions = (req,res,next) => {
+exports.addTransactions = async (req,res,next) => {
     res.send('add transactions')
 }
 
 //@desc  Delete transaction
 //@route GET /api/v1/transactions/:id
 //@access Public
-exports.deleteTransactions = (req,res,next) => {
+exports.deleteTransactions = async (req,res,next) => {
     res.send('delete transactions')
 }
