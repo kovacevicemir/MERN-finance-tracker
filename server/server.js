62668eb7a1a53bead8617f720a1fd0101091802e
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-04 02:52:10 +1000

    Build ready, server static file ready for production

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const path = require('path')
 
 dotenv.config({path: './config/config.env'})
 
@@ -18,6 +19,12 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 app.use('/api/v1/transactions', transactions)
+
+if(process.env.NODE_ENV === 'production'){
+     app.use(express.static('../client/build'))
+     app.get('*', (req, res) => res.sendfile(path.resolve('client'
+     ,'build', 'index.html')))
+}
 
 
 const PORT = process.env.PORT || 5000
