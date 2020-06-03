d3105aed6b79b45b98fd522bd873bdb780453480
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-03 13:21:30 +1000

    Provider component added

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -11,3 +11,14 @@ const initialState = {
 }
 
 export const GlobalContext = createContext(initialState)
+
+//Provider Component
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
