ffe76442a8a4b26907010f33315d5f64b21e2131
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-04 01:11:31 +1000

    delete action/reducer implementation

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -8,7 +8,6 @@ export default function Balance() {
     const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-    console.log(transactions)
     return (
         <>
             <h4>Your Balance</h4>
