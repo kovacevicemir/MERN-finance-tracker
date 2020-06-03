69e889ae3d9b36211b7d49b09e5b604264e63273
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-03 13:09:20 +1000

    context created

--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function IncomeExpenses() {
     return (
-        <div class="inc-exp-container">
+        <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
                 <p className="money plus">+$0.00</p>
--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -6,7 +6,7 @@ export default function TransactionList() {
             <h3>History</h3>
             <ul id="list" className="list">
                 <li className="minus">
-                Cash <span>-$400</span><button class="delete-btn">x</button>
+                Cash <span>-$400</span><button className="delete-btn">x</button>
                 </li>
             </ul>
         </div>
